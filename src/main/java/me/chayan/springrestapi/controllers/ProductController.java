2434b9bdc09deca976d4b4a256dd035e130d8e97
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-11-18 09:47:29 +0600

    Product API CREATED

--- src/main/java/me/chayan/springrestapi/controllers/ProductController.java
+++ src/main/java/me/chayan/springrestapi/controllers/ProductController.java
@@ -1,16 +1,46 @@
 package me.chayan.springrestapi.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.chayan.springrestapi.models.Product;
+import me.chayan.springrestapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
-@RequestMapping("/product")
 @CrossOrigin("*")
 public class ProductController {
 
-    @RequestMapping("/")
-    public String getHelloWorld() {
-        return "Hello World";
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping(value = {"/product/create" }, method = POST)
+    public Product create(@RequestBody Product entity){
+        return this.service.create(entity);
     }
+
+    @RequestMapping(value = {"/product/get/{id}" }, method = GET)
+    public Product getProduct(@PathVariable Long id){
+        return this.service.getProduct(id);
+    }
+
+    @RequestMapping(value = {"/product/get" }, method = GET)
+    public List<Product> getProducts(){
+        return this.service.getProducts();
+    }
+
+    @RequestMapping(value = {"/product/edit" }, method = PUT)
+    public Product edit(@RequestBody Product editEntity){
+        return this.service.edit(editEntity);
+    }
+
+    @RequestMapping(value = {"/product/delete/{id}" }, method = DELETE)
+    public ResponseEntity<?> deleteSimBillTransaction (@PathVariable(name = "id") Long id){
+        return this.service.delete(id);
+    }
+
+
 }
--- src/main/java/me/chayan/springrestapi/service/ProductService.java
+++ src/main/java/me/chayan/springrestapi/service/ProductService.java
@@ -1,7 +1,53 @@
 package me.chayan.springrestapi.service;
 
+import me.chayan.springrestapi.exception.ResourceNotFoundException;
+import me.chayan.springrestapi.models.Product;
+import me.chayan.springrestapi.payload.response.BaseResponse;
+import me.chayan.springrestapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public Product create(Product entity) {
+        return this.repository.save(entity);
+    }
+
+    public Product getProduct(Long id) {
+        return this.repository.findById(id).get();
+    }
+
+    public List<Product> getProducts() {
+        return this.repository.findAll();
+    }
+
+    public Product edit(Product editEntity) {
+
+        Optional<Product> dbEntityInstOp = this.repository.findById(editEntity.getId());
+
+        if (dbEntityInstOp.isPresent()) {
+            Product dbEntityInst = dbEntityInstOp.get();
+            dbEntityInst.setName(editEntity.getName());
+            dbEntityInst.setPrice(editEntity.getPrice());
+            dbEntityInst.setImageUrl(editEntity.getImageUrl());
+            dbEntityInst.setAvailable(editEntity.isAvailable());
+            return this.repository.save(dbEntityInst);
+        }
+
+        return null;
+    }
+
+    public ResponseEntity<?> delete(Long id) {
+        Product deleteInst = repository.findById(id).orElseThrow(()
+                -> new ResourceNotFoundException("Resource not found for this id :: " + id));
+        repository.delete(deleteInst);
+        return ResponseEntity.ok(new BaseResponse(true,"Successfully Deleted Data"));
+    }
 }
